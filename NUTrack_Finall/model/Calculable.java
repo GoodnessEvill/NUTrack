50b476f56d73600672feeaa5a3f076f3834d91f6
Barış Bideci <67412264+GoodnessEvill@users.noreply.github.com>
2025-05-23 23:48:50 +0300

    Delete NUTrack_Finall directory

--- NUTrack_Finall/model/Calculable.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-// This interface means that a class can calculate calories
-public interface Calculable {
-    // Method that must return calculated calories
-    int calculateCalories();
-}
